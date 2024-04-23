288da287774ecabe31f7438f4ecf5d80816bdc4d
samishken <samishken@gmail.com>
2024-04-22 21:22:35 -0400

    update aws region

--- eks/eks.tf
+++ eks/eks.tf
@@ -118,7 +118,7 @@ resource "aws_iam_instance_profile" "worker" {
 
 ###############################################################################################################
 resource "aws_eks_cluster" "eks" {
-  name = "valaxy-dev"
+  name = "sam-test-cluster"
   role_arn = aws_iam_role.master.arn
 
   vpc_config {
